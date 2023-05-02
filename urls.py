7f9332262d1a7df12c64255609a9d00bcdceee22
Arjun Suresh Kumar <110417465+arsureshkumar@users.noreply.github.com>
2023-05-02 16:02:53 -0700

    added comma
    comma

--- urls.py
+++ urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views 
 urlpatterns = [
 
-    path('templates/index/',views.about,name='index')
+    path('templates/index/',views.about,name='index'),
     path('templates/about/',views.about,name='about'),
     path('templates/home/',views.home_view,name='home'),
     path('templates/register/',views.register,name='register'),
