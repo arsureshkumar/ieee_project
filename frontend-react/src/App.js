940156176fcff031ca7d803bd6a4a75ec83b61e2
Hou Wai Wan <wanhouwai@gmail.com>
2023-05-09 20:28:22 -0700

    changed to react

---
+++ frontend-react/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Webcam from "react-webcam"
+
+function App() {
+  return (
+    <div className="App">
+      <body>
+        <h1> FaceDrive PogU </h1>
+        <Webcam />
+      </body>
+    </div>
+  );
+}
+
+export default App;
